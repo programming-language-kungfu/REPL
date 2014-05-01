bdbd13b686d30338e1110b49c3f6868a42f01146
Kisitu Augustine <austiine04@gmail.com>
2014-05-01 15:52:30 +0300

    parsing for user input instead of just echoing it back

--- main.c
+++ main.c
@@ -27,6 +27,20 @@ void add_history(char * unused ){}
 
 int main(int argc, char ** argv){
 
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expression = mpc_new("expression");
+    mpc_parser_t* Lispy = mpc_new("lispy");
+
+    mpca_lang(MPCA_LANG_DEFAULT,
+            "
+            number    : /-?[0-9]+/;                                   \
+            operator  : '+' | '-' | '*' | '/';                        \
+            expression: <number> | '(' <operator> <expression>+ ')';  \
+            lispy     : /^/ <operator> <expression>+ /$/;             \
+            ",
+            Number, Operator, Expression, Lispy);
+
     puts("Lispy Version 0.0.0.0.1");
     puts("Press Ctrl+c to Exit\n");
 
@@ -34,7 +48,17 @@ int main(int argc, char ** argv){
         char * input = readline("lispy> ");
         add_history(input);
 
-        printf("No you are a %s\n", input);
+        mpc_result_t result;
+        if(mpc_parse("<stdin>", input, Lispy, &result)){
+            mpc_ast_print(result.output);
+            mpc_ast_delete(result.output);
+        }else{
+            mpc_err_print(result.error);
+            mpc_err_delete(result.error);
+        }
+
         free(input);
     }
+
+    mpc_cleanup(4, Number, Operator, Expression, Lispy);
 }
